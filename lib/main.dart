fab2402aa1b359dcca7b5f5833ec3dab00dbe0e7
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-01-05 21:31:37 -0500

    setstate

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,53 @@ void main() {
   );
 }
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int x = 100;
+  int y = 200;
+
   @override
   Widget build(BuildContext context) {
+    y++;
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                x.toString(),
+                style: TextStyle(fontSize: 50),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  x = 300;
+                  setState(() {});
+                  print(x);
+                },
+                child: Text("Cambiar valor"),
+              ),
+              Container(
+                height: 200,
+                color: Colors.red,
+                child: Column(
+                  children: [
+                    Text(
+                      y.toString(),
+                      style: TextStyle(fontSize: 50),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
